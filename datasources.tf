831b330ab22153275bb11b039f9915f63ee3cd08
judigot <judigot@github.com>
2023-03-08 15:55:12 +0800

    Commit

--- datasources.tf
+++ datasources.tf
@@ -3,6 +3,11 @@ data "aws_ami" "ubuntu18" {
   owners      = ["099720109477"]
 
   filter {
+    name   = "image-id"
+    values = ["ami-0a97be4c4be6d6cc4"]
+  }
+
+  filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"] # * to use the latest
   }
