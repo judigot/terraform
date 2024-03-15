0d6ff5cdba1db4574447f6206b3e8f63ed5d9836
judigot <judigot@gmail.com>
2024-03-15 14:50:10 +0800

    Update

--- providers.tf
+++ providers.tf
@@ -10,7 +10,9 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
-  shared_config_files      = data.external.os_check.result["is_windows"] == "true" ? ["~/.aws/config"] : []
-  shared_credentials_files = data.external.os_check.result["is_windows"] == "true" ? ["~/.aws/credentials"] : []
-  profile                  = "admin" # [default] profile name contained in ~/.aws/credentials
+
+  # Comment these when using AWS CloudShell
+  # shared_config_files      = data.external.os_check.result["is_windows"] == "true" ? ["~/.aws/config"] : []
+  # shared_credentials_files = data.external.os_check.result["is_windows"] == "true" ? ["~/.aws/credentials"] : []
+  # profile                  = "admin" # [default] profile name contained in ~/.aws/credentials
 }
