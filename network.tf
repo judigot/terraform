5e9262f2ca41df35e82675a28e0aaad669063c28
judigot <judigot@gmail.com>
2024-03-15 10:59:31 +0800

    Update

--- network.tf
+++ network.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  # availability_zone       = "${var.region}a" // Specify AZ if needed, uncomment as necessary.
+  availability_zone       = "${var.region}a" // Specify AZ if needed, uncomment as necessary.
 
   tags = {
     Name = "dev-public"
@@ -53,7 +53,7 @@ resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  # availability_zone       = "${var.region}b" // Specify AZ if needed, uncomment as necessary.
+  availability_zone       = "${var.region}c" // Specify AZ if needed, uncomment as necessary.
 
   tags = {
     Name = "dev-public"
