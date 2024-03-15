8ef81be55b1f08ec1183d48c2a3679501fae4699
judigot <judigot@gmail.com>
2024-03-15 15:17:05 +0800

    Update

--- variables.tf
+++ variables.tf
@@ -28,8 +28,8 @@ variable "username" {
 
 variable "region" {
   type    = string
-  default = "us-east-1" # N. Virginia
-  # default = "us-east-2" # Ohio
+  # default = "us-east-1" # N. Virginia
+  default = "us-east-2" # Ohio
   # default = "us-west-2" # Oregon
   # default = "ap-southeast-1" # Singapore
 }
