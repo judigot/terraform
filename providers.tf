e06df851ba092dfb38fb8e01b4f81394e49b69bf
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2026-01-20 19:36:07 +0800

    Update

--- providers.tf
+++ providers.tf
@@ -10,9 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
-
-  # Comment these when using AWS CloudShell
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "admin" # [default] profile name contained in ~/.aws/credentials
 }
