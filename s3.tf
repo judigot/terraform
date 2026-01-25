5df668cb39b84d5aa20807417952580ef3da89f3
Jude Francis <judigot@gmail.com>
2026-01-25 22:16:17 +0800

    chore: remove import block after successful import

--- s3.tf
+++ s3.tf
@@ -3,11 +3,6 @@
 # Put this file in the same folder as your other *.tf files (e.g., main.tf).
 # Also put Init.ps1 in the same folder so ${path.module}/Init.ps1 resolves.
 
-import {
-  to = aws_s3_bucket.bootstrap
-  id = "apportable-bootstrap-init-ps1"
-}
-
 resource "aws_s3_bucket" "bootstrap" {
   bucket = "apportable-bootstrap-init-ps1"
 }
